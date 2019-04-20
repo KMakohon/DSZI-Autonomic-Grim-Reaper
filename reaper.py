2fdd4ca9d31c11c7d938b89bde8c72a75fd7f160
MaciejGrochowski <32848261+MaciejGrochowski@users.noreply.github.com>
2019-04-20 03:04:12 +0200

    Control Reaper by mouse
    alpha mouse-control reaper - reaper doesn't know how to deal with walls

--- reaper.py
+++ reaper.py
@@ -1,7 +1,7 @@
-from settings import TILESIZE, PLAYER_SPEED, pg, PLAYER_HIT_RECT
+from settings import TILESIZE, PLAYER_SPEED, PLAYER_HIT_RECT
 from collisions import *
-
 vec = pg.math.Vector2
+from math import floor
 
 
 class Reaper(pg.sprite.Sprite):
@@ -43,3 +43,46 @@ class Reaper(pg.sprite.Sprite):
         self.hit_rect.centery = self.pos.y
         collide_with_walls(self, self.game.walls, 'y')
         self.rect.center = self.hit_rect.center
+        #print(str(self.game.agent.pos / 64))
+        #if(collide_with_people(self, self.game.people, 'x') or collide_with_people(self, self.game.people, 'y')):
+           #self.game.drawPeople =  <- kolizje z ludźmi, na kiedyś
+
+    def go_to(self, game, newpos):
+        self.target = vec(0,0)
+        self.target.x = newpos[0]
+        self.target.y = newpos[1]
+        licznik  = 0
+        self.pos.y = floor(self.pos.y)
+        self.pos.x = floor(self.pos.x)
+        self.target.x = self.target.x - game.camera.x
+        self.target.y = self.target.y - game.camera.y
+        oldposx = 0
+        oldposy = 0
+        print(game.camera.x)
+        while(True):
+            if(oldposx == self.pos.x and oldposy == self.pos.y):
+                break
+            oldposx = self.pos.x
+            oldposy = self.pos.y
+            if(licznik>1000):
+                break
+            if self.target.x == self.pos.x and self.target.y == self.pos.y:
+                break
+            if self.target.x > self.pos.x:
+                self.pos.x += 1
+                #prawo
+            if self.target.x < self.pos.x:
+                self.pos.x -= 1
+                #lewo
+            if self.target.y > self.pos.y:
+                self.pos.y += 1
+                #dol
+            if self.target.y < self.pos.y:
+                self.pos.y -= 1
+                #gora
+            print("Po zmianie pozytcja to", self.pos)
+            game.update()
+            game.draw()
+
+
+
