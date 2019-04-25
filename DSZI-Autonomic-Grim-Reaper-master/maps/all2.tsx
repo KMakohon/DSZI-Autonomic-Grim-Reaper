24d7934a9d47e52ac61e52ef73622bee1444a5a7
zofiadob <ikarani24@op.pl>
2019-04-25 15:51:13 +0200

    a

--- DSZI-Autonomic-Grim-Reaper-master/maps/all2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="all2" tilewidth="64" tileheight="64" spacing="10" margin="10" tilecount="12" columns="4">
- <image source="all.png" width="306" height="232"/>
-</tileset>
