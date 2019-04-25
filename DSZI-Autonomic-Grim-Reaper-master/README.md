b076231de259dc9c3fb95fc1c163c15575a385fa
zofiadob <ikarani24@op.pl>
2019-04-25 14:23:06 +0200

    Adding

---
+++ DSZI-Autonomic-Grim-Reaper-master/README.md
@@ -0,0 +1,69 @@
+﻿# DSZI-Autonomic-Grim-Reaper
+
+### Autorzy: Zofia Dobrowolska, Maciej Grochowski, Katarzyna Makohon
+
+"Autonomiczna Kostucha" jest agentem poruszającym się po dwuwymiarowym środowisku. Jej zadaniem jest selekcjonowanie osób, które powinna zabić, oraz ustalenie optymalnej trasy, która pozwoli jej efektywnie wypełniać swoja rolę. Kostucha podejmuje decyzje, co do tego kogo i w jakiej kolejności zabić na podstawie cech indywidualnych osób (np. wiek)
+
+---
+
+### Autonomic Grim Reaper - Środowisko Agenta
+
+Data: 24.03.2019
+
+#### 1. Wstęp
+
+W tym przyroście skupiliśmy się na stworzeniu środowiska dla agenta, oraz struktury klas wykorzystywanej w projekcie. Stworzyliśmy proste środowisko 2D stylizowane na 64Bit RPG oraz 5 klas: Game, Reaper, Person, Wall, TiledMap oraz Camera.
+
+#### 2. Wykorzystane narzędzia
+
+Projekt został napisany w języku Python3 przy użyciu modułu PyGame, aby wygenerować graficzną reprezentację klas, oraz umożliwić wizualizację działania agenta. Celem randomizacji parametrów wykorzystano moduł random. 
+
+Mapa została dodana z pomocą modułu PyTMX oraz programu "Tiled", by swobodnie manipulować stworzonymi na potrzeby gry grafikami.
+
+#### 3. Reprezentacja wiedzy
+
+Szersze omówienie działania programu wymaga większego skupienia się na kodzie źródłowym.
+
+- **Game:**
+
+  Zawiera podstawowe ustawienia wyświetlania okna (wysokość, szerokość), informacje o miejscach, z których należy pobrać grafiki wykorzystywane w grze, oraz 6 metod: new(), run(), quit(), update(), draw() oraz events().
+
+  * Metoda new() - Odpowiada za stworzenie obiektów wykorzystywanych w grze tj. Reaper, Person oraz Camera.
+
+  * Metoda run() - Uruchamia metody events, update oraz draw w nieskończonej pętli.
+
+  * Metoda quit() - Odpowiada za zamknięcie gry i zakończenie programu. 
+
+  * Metoda update() - Uruchamia metody update wszystkich sprite'ów celem zaktualizowania ich pozycji. 
+
+  * Metoda draw() - Odpowiada za umieszczenie na mapie sprite'ów.
+
+  * Metoda events() - W obecnym kształcie sprawdza czy użytkownik nie chce wyłączyć programu. Gdy użytkownik wciśnie klawisz Escape, lub zamknie okno programu, uruchamia metodę quit().
+
+- **Reaper:**
+
+  Jest obiektem zawierającym sprite postaci agenta. Posiada paramtery pozwalajace umieścić go w przestrzeni oraz informacje odnośnie gry. Posiada metodę get_keys() oraz update().
+
+  * Metoda get_keys() - Oczekuje na event (tj. naciśnięcie przez gracza któregoś z klawiszy strzałek) a następnie tworzy wektor wykorzystywany przez metodę update() do zmiany pozycji agenta w przestrzeni. W przypadku zmiany kierunku ruchu zmienia również obrazek przypisany agentowi.  
+
+  * Metoda update() - zmienia paramtery agenta i jego pozycję na mapie. 
+
+- **Person:**
+
+  Jest to obiekt zawierający sprite postaci z którą agent w przyszłości wejdzie w interakcję. Posiada ona parametry pozwalające umieścić ją w środowisku agenta, oraz parametry opisujące wiek, choroby, charakter i płeć. Postać nie ma opcji poruszenia się.
+
+- **Wall:**
+
+  Klasa określa obiekty, które agent będzie postrzegał jako przeszkody.  Posiada parametry umieszczające przeszkodę w przestrzeni oraz parametry określające wysokość i szerokość przeszkody. 
+
+- **TiledMap:**
+
+  Jest to klasa tworząca mapę na podstawie przygotowanego wcześniej w programie "Tiled" pliku.
+
+- **Camera:**
+
+  Jest to klasa stworzona po to, aby postać agenta zawsze była widoczna na ekranie. Posiada ona parametry pozwalające umiejscowić ją w środowisku oraz metody pozwalające zmieniać jej pozycję. 
+
+---
+
+
---
+++ DSZI-Autonomic-Grim-Reaper-master/reaper.py
@@ -0,0 +1,109 @@
+from settings import TILESIZE, PLAYER_SPEED, PLAYER_HIT_RECT
+from collisions import *
+from math import floor
+
+
+vec = pg.math.Vector2
+
+
+class Reaper(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.reaper
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img_R
+        self.rect = self.image.get_rect()
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
+        self.pos = vec(x, y) * TILESIZE
+        self.countsteps = 0
+        self.target = vec(0, 0)
+
+    def get_keys(self):
+        self.vel = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_UP]:
+            self.vel = vec(0, -PLAYER_SPEED)
+        if keys[pg.K_DOWN]:
+            self.vel = vec(0, PLAYER_SPEED)
+        if keys[pg.K_RIGHT]:
+            self.vel = vec(PLAYER_SPEED, 0)
+            if (self.image == self.game.player_img_L):
+                self.image = self.game.player_img_R
+        if keys[pg.K_LEFT]:
+            self.vel = vec(-PLAYER_SPEED, 0)
+            if (self.image == self.game.player_img_R):
+                self.image = self.game.player_img_L
+
+    def update(self):
+        self.get_keys()
+        self.rect.center = self.pos
+        self.pos += self.vel
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, 'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, 'y')
+        self.rect.center = self.hit_rect.center
+
+    def go_to(self, game, newpos):
+        self.target = vec(newpos[0], newpos[1])
+        self.pos.y = floor(self.pos.y)
+        self.pos.x = floor(self.pos.x)
+        self.target.x = self.target.x - game.camera.x
+        self.target.y = self.target.y - game.camera.y
+        self.target.x = (self.target.x - self.target.x % 16) + 16
+        self.target.y = (self.target.y - self.target.y % 16) + 16
+        oldposx = 0
+        oldposy = 0
+        while(True):
+            if(oldposx == self.pos.x and oldposy == self.pos.y):
+                break
+            oldposx = self.pos.x
+            oldposy = self.pos.y
+            if self.target.x == self.pos.x and self.target.y == self.pos.y:
+                break
+            if self.target.x > self.pos.x:
+                self.pos.x += 1
+                if self.target.x - self.pos.x > 16 and (self.image == self.game.player_img_L):
+                    self.image = self.game.player_img_R
+                    self.countsteps += 1
+                #prawo
+            if self.target.x < self.pos.x:
+                self.pos.x -= 1
+                if self.pos.x - self.target.x > 16 and self.image == self.game.player_img_R:
+                        self.image = self.game.player_img_L
+                        self.countsteps += 1
+                #lewo
+            if self.target.y > self.pos.y:
+                self.pos.y += 1
+                #dol
+            if self.target.y < self.pos.y:
+                self.pos.y -= 1
+                #gora
+            hits = pg.sprite.spritecollide(self, self.game.people, True, collide_hit_rect)
+            if hits:
+                for sprite in hits:
+                    sprite.banish()
+            grasses = pg.sprite.spritecollide(self, self.game.grasses, False, grass_collide)
+
+            if grasses:
+                print("Trawa")
+            roads = pg.sprite.spritecollide(self,self.game.roads, False, grass_collide)
+
+            if roads:
+                print("Droga")
+
+            indoors = pg.sprite.spritecollide(self, self.game.indoors, False, grass_collide)
+            if indoors:
+                print("Wnetrze")
+
+            dirt = pg.sprite.spritecollide(self, self.game.dirt, False, grass_collide)
+            if dirt:
+                print("Ziemia")
+
+            self.countsteps += 1
+            game.update()
+            game.draw()
+        self.countsteps -= 1
